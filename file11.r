70bae1834bd559391b9c621b8c3ec54deb1d233b
Aqsa Zulfiqar <aqsazportfolio@gmail.com>
2022-03-01 12:27:45 +0500

    done

--- file11.r
+++ file11.r
@@ -25,3 +25,36 @@ f <- function(a= 10, b=2)
   print(b)
 }
 
+install.packages("RSQLite")
+data("DBfile.db")
+data('data.xlsx')
+
+
+
+activity <- data.frame(opid=c("OP1", "OP2", "OP3", "OP4", "OP5", "OP6", "OP7"))
+names <- data.frame(operator=c("pak", "plus", "sub", "minus", "carlly", "moe", "bye"))
+blended <- cbind(activity, names)
+
+blended
+
+
+
+
+df1 <- data.frame(LETTERS, dfindex = 1:26)
+df2 <- data.frame(letters, dfindex = c(1:10,15,20, 22:35))
+merge(df1, df2, all = TRUE)
+
+
+##tidyverse
+
+install.packages("tidyverse")
+
+head(msleep)
+groups <- group_by(msleep, genus)
+levels(groups$genus)
+selecting <- select(msleep, startsWith("A"))
+
+
+
+td <- data.frame(x =c("a:1", "a:2", "c:4", "d:4"))
+td
